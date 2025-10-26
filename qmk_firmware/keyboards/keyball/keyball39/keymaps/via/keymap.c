d5fab030f3294b0477504d1499a7f0bb0df1593d
Seiya <seiya8bit@outlook.com>
2025-10-27 08:13:04 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q            , KC_W           , KC_E           , KC_R           , KC_T           ,                                   KC_Y           , KC_U           , KC_I           , KC_O           , TD(P_LAYR)     ,
     KC_A            , KC_S           , KC_D           , KC_F           , KC_G           ,                                   KC_H           , KC_J           , KC_K           , KC_L           , LT(5, KC_ENT)  ,
     KC_Z            , KC_X           , KC_C           , KC_V           , KC_B           ,                                   KC_N           , KC_M           , KC_BSPC        , KC_DEL         , KC_TAB         ,
-    LSFT_T(KC_ESC)  , KC_PSCR        , KC_LGUI        , MO(3)          , KC_LALT        , MO(2)          , LT(1, KC_SPC)  , KC_RCTL        , _______        , _______        , _______        , KC_RSFT
+    KC_ESC          , KC_PSCR        , KC_LGUI        , KC_LSFT        , KC_LCTL        , MO(1)          , MO(2)          , RALT_T(KC_SPC) , _______        , _______        , _______        , MO(3)
   ),
 
   // Number/Functions
