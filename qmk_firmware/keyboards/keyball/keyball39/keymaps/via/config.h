7f5042f87baacea50883ded2f5f57304c8db3fbc
Seiya <seiya8bit@outlook.com>
2025-10-27 08:33:13 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
-#define AUTO_MOUSE_TIME 250
+#define AUTO_MOUSE_TIME 300
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
